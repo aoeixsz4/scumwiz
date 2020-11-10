18dd5ed94047d7f0faba1a5881b9f0de43f38515
Joanna Doyle <jjadoyle@gmail.com>
2020-11-10 20:58:28 +0100

    Add detailed usage text and cli flags for tuning
    Change a lot of things quite substantially.
    Add a usage text detailing required setup with screen & options
    that must be set in your player rcfile, etc., things the script
    assumes for simplicity and it won't work properly without.
    
    Add a mechanism for tuning scumming behaviour using CLI flags.
    Random wand in first room trumps all the other targets that can be set.
    
    The processing of curses screendumps is still a bit crude, eventually
    I'd like to have the script use the boxes to separate out the top left
    messages area, the map, the status lines, and the perm_invent box over
    to the right.
    
    It works pretty well atm tho.
    
    Signed-off-by: Joanna Doyle <jjadoyle@gmail.com>

--- README.md
+++ README.md
@@ -1,22 +1,8 @@
 # scumwiz
 Perl script for automated NetHack startscumming
 
-Requirements: SSH, GNU Screen & Perl
-Setup:
-  before: set windowtype:curses in your nethack rcfile, and enable perm_invent
-  
-  also define race/role/gender/alignment in your rcfile - script assumes this is set
-  
-  edit the player name in scumwiz.pl to reflect your own login name
-  
-  start a named screen session (default session name in scumwiz.pl is Halea)
-  
-  connect to hardfought nh server via ssh within the named screen session
-  
-  log in to your account
-  
-  run ./scumwiz.pl in another terminal emulator
+Requirements: SSH, GNU Screen & Perl incl. perl modules List::MoreUtils and Time::HiRes.
+I really need to write a more detailed readme, but scumwiz.pl now has a fairly detailed
+usage text if you run it with the flag -h or -help
 
-Scumwiz.pl will keep re-rolling wizards until some favourable parameters are arrived at,
-current configuration is to stop if: int >= 16 && have_polyitem && (have_marker || have teleport_control)
-script will also stop if a wand is seen in the first room - check for WoW manually
+This will explain how to run it, and some things you need to have set up in your rcfile.
--- scumwiz.pl
+++ scumwiz.pl
@@ -2,142 +2,121 @@
 
 use strict;
 use warnings;
-use Time::HiRes qw( usleep gettimeofday tv_interval);
+use List::MoreUtils qw(uniq any none);
+use Time::HiRes qw(usleep gettimeofday tv_interval);
 use POSIX qw(mkfifo);
 
-my $session = 'Halea';
-my $user = 'Aoei';
-my $user_lowercase = 'aoei';
-my $t0 = [gettimeofday];
-my $count = 0;
-my $timeout = 0;
-my @old_data;
-my @tools;
-my @weapons;
-my @levelmap;
-my @coords; # x, y Yo
-my @first_room_features;
+# use environment-defined TMPDIR, if it exists
+# the default of /tmp should be fairly cross-platform tho
+my $tmp_dir = $ENV{TMPDIR} // '/tmp';
 
-while (1)
-{
-	my $tonal;
-	my $poly;
-	my $teleport_control;
-	my $marker;
-	my $intelligence;
-	my $context;
-	my $slow_digestion;
-	my $i;
-	my $j;
-	my @data;
+# define a usage text thingy here
+my $run_as = $0;
+my $usage_text = <<EOF;
+Joanna aka aoei's super cool startscum script for NetHack wizards.
+ (C) 2020 Joanna Doyle - treat as if licensed under MIT.
+There is no warranty for this software, nor do I accept any responsibility
+for your splats. Current version is designed specifically for TNNT.
 
-	# get screen
-	do
-	{
-		usleep 500000;
-		#@old_data = @data
-		#	if (@data);
-		@data = get_screencopy ($session)
-	}
-#	while (!@data || diff_array (\@data, \@old_data));
-	while (!@data);
-
-	# start game
-	if (grep { /Connection closed by foreign host/ } @data) {
-		my $elapsed = get_elapsed ($t0);
-		die "Spamfiltered ($count tries, time: $elapsed."
-	}
+Before running the script, you need to be logged in to hardfought
+in the active window of a screen session, and in the TNNT submenu.
+The size of the screen window / terminal window needs to be fairly large,
+so that all starting inventory items fit on a single page - 40x150 is what I use.
+Your rcfile must include certain options, required by the script,
+many of these are set in order to cut down on unnecessary prompts etc:
 	
-	if (grep { /THE NOVEMBER NETHACK TOURNAMENT IS LIVE/ }  @data)
-	{
-		system ('screen', '-S', $session, '-X', 'stuff', 't');
-	} elsif (grep { /^.?>>/ } @data) {
-		system ('screen', '-S', $session, '-X', 'stuff', ' ');
-	} elsif (grep { /Do you want to see the dungeon overview/ } @data) {
-		system ('screen', '-S', $session, '-X', 'stuff', 'q');
-	} elsif (grep { /Beware, there will be no return/ } @data) {
-		system ('screen', '-S', $session, '-X', 'stuff', 'y');
-#	elsif (grep { /\ba - a blessed \+1 quarterstaff\b/ } @data && grep { /\(end\)/ } @data)
-	# perm_invent means we don't need to actually open inventory so it'll be there on the welcome screen
-	# the welcome message remains on screen during quit steps, so process this case last
-	} elsif (grep { /Hello ${user_lowercase}/ } @data) {
-		# max scum one game per second otherwise dgl error turfs us out
-		# tyrec/2020-10-29.08:51:59.ttyrec.gz already exists; do you wish to overwrite (y or n)? 
-		if (grep { /\// } @data)
-		{ goto OUT }
-        elsif (grep { /\w - a .*wand/ } @data)
-        { goto OUT }
-		# check for key items
-		foreach my $line (@data) {
-			if ($line =~ /$user the Evoker/) {
-				$intelligence = $1
-					if ($line =~ /\bSt:\d+ Dx:\d+ Co:\d+ In:(\d+) Wi:\d+ Ch:\d+\b/);
-				last;
-			}
-			if ($line =~ /(tooled horn|harp|bugle|flute)/) {
-				$tonal = $1;
-			}
-			if ($line =~ /teleport control/) {
-				$teleport_control = 1;
-			}
-		}
-		$poly = 1
-			if (grep { /(?:ring of polymorph control|wand of polymorph)/ } @data);
-		$marker = 1
-			if (grep { /magic marker \(0:[6789]/ } @data);
-		$slow_digestion = 1
-			if (grep { /ring of slow digestion/ } @data);
-		my $log_line = "scum$count; int: $intelligence, ";
-		if ($tonal) {
-			$log_line .= "got $tonal, ";
-		} else {
-			$log_line .= "no tonal, ";
-		}
-		if ($poly) {
-			$log_line .= "got polyitem";
-		} else {
-			$log_line .= "no polyitem";
-		}
-		if ($marker && $teleport_control) {
-			$log_line .= ", marker and TC!";
-		} elsif ($marker) {
-			$log_line .= ", marker!";
-		} elsif ($teleport_control) {
-			$log_line .= ", TC!";
-		}
-		print "$log_line\n";
+	# these first two lines are absolutely necessary
+	OPTIONS=!legacy,scores:!t !a !o,!tombstone,disclose:-i -a -v -g -c -o
+	OPTIONS=windowtype:curses,perm_invent
+	# the combo doesn't *have* to be Wiz-Elf-Fem-Cha, just needs to be set
+	OPTIONS=role:wizard,race:elf,gender:female,align:chaotic
+	# not 100% whether these are all required, but add just to be sure
+	OPTIONS=!cmdassist,!help,!verbose,suppress_alert:3.4.3,!splash_screen
+	# it's also assumed you have autopickup enabled and / in types
+	# (or an autopickup exception that will pick up unidentified wands)
+	
+	 (the exact combo doesn't necessarily have to be Wiz-Elf-Fem-Cha,
+	  but it absolutely has to be defined in the rcfile for the script to work)
 
-		goto OUT
-			if (    ($intelligence && $intelligence >= 17)
-				 #&& $tonal
-				 && $poly
-				 && ($marker || $teleport_control));
-		$count++;
-		undef @levelmap;
-		undef @coords;
-		undef @first_room_features;
-		undef @weapons;
-		undef @tools;
-		system ('screen', '-S', $session, '-X', 'stuff', "<");
-	} elsif (grep { /gzip: .*\.ttyrec\.gz/ } @data) {
-		# if this comes up something weird happened
-		print "got weird gzip ttyrec question from server\n";
-		exit(1);
-	} else {
-		$timeout++;
-		if ($timeout > 15 * 5)
-		{
-			my $elapsed = get_elapsed ($t0);
-			print @data;
-			die "Timed out ($count tries, time: $elapsed)\n"
-		}
-	}
+	Usage:
+	\$ $run_as [-h|-help] [-user=<dgl_login>] [-session=<screen_id>]
+				[-<target>...] [no<target>...] [+<target>...]
+
+	-h/-help	- print this text and quit
+
+	-session=	- define the name of the relevant screen session
+					(defaults to 'default').
+
+	-<target>	- add a hard scumming target,
+					(defaults are 'int', 'tonal' and 'poly')
+					a hit requires all hard targets to be found.
+
+	no<target>	- remove one of the default hard targets.
+
+	+<target>	- add a soft scumming target - if any soft targets
+					are set, a hit is only called if at least one
+					of the defined soft targets is found.
+	
+NB: all hard and soft targets are ignored if a random wand is found.
+Possible <target>s to scum for include: 'int' (meaning >=17), 'tonal',
+	'poly' (either polywand or polycontrol), 'tc' (teleport control),
+	'digging' (start with a digging wand), 'foodless' (slow digestion),
+	'marker' (start with a marker).
+EOF
+
+# take two array refs, first is an exclusion list
+# second is the arrray to be filtered
+# return a list only containing values that 
+# don't appear in the exclusion set, strings
+# have to match exactly to be excluded
+sub negate_filter {
+    my ($exclusion_spec, $input_array) = @_;
+    my @filtered_list = ();
+
+    foreach my $list_item (@$input_array) {
+        if (none { $_ eq $list_item } @$exclusion_spec) {
+            push @filtered_list, $list_item;
+        }
+    }
+    return @filtered_list;
 }
 
-OUT:
-my $elapsed = get_elapsed ($t0);
-print "Success ($count tries, time: $elapsed).\n";
-exit 0;
+# return true if the second array ref contains
+# all the elements of the first as a subset of it
+# this bit is used to test whether the set of
+# goodies provided by the current game includes
+# the set of hard scum targets
+sub is_subset {
+    my ($possible_subset, $search_space) = @_;
+
+    # we can do a neat trick with the negate_filter,
+    # $search_space becomes our exclusion_spec, with
+    # $possible_subset as the input array -
+    # if negate_filter returns an empty array,
+    # we return true
+    if (negate_filter ($search_space, $possible_subset) == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+# count how many items in the first array
+# appear in the second one, we can also use negate_filter
+# here and take the difference in array length as hit count,
+# however that seems odd and counter intuitive,
+# will simply do the simple thing
+sub count_hits {
+    my ($test_set, $search_space) = @_;
+    my $count = 0;
+
+    foreach my $test_item (@$test_set) {
+        if (any { $_ eq $test_item } @$search_space) {
+            $count++;
+        }
+    }
+    return $count;
+}
 
 sub get_elapsed
 {
@@ -174,11 +153,14 @@ sub get_elapsed
 	return join ' ', @unit
 }
 
+# returns a line by line dump from the active
+# window of the screen session we're interested in,
+# with a bit of processing to trim excess whitespace etc.
 sub get_screencopy
 {
 	my ($fh, @screen_raw, @data_out, $i, $signal);
 	my $session = shift;
-	my $filename = "/tmp/$session.$$";
+	my $filename = "${tmp_dir}/${session}.$$";
 	mkfifo ($filename, 0700)
 		or die "Couldn't make named pipe";
 	my $pid = fork;
@@ -199,53 +181,263 @@ sub get_screencopy
 	close $fh;
 	unlink $filename;
 
-#	# post-processing - truncate tailing empty lines (previous solution removed all tempty lines which confused other things)
-#	# blank screen causes hang so check for that too
-#	chomp (@data);
-#	$i = 0; $signal = 0;
-#	while ($i < @data)
-#	{ if ($data[$i]) { $signal = 1; $i = @data } $i++ }
-#	return
-#		unless $signal;
-#	$i = -1;
-#	while (!$data[$i]) { $i-- }
-#	splice (@data, $i + 1);
 	foreach my $line (@screen_raw) {
 		$line =~ s/^\s*//; # strip preceeding whitespace
 		$line =~ s/\s*$//; # strip tailing whitespace
 		if (length($line) > 0) {
 			push @data_out, $line;
-			print STDERR "$line\n";
+            #print STDERR "$line\n";
 		}
 	}
 	return @data_out;
 }
 
-sub diff_array
+## i think this subroutine isn't currently in actual use
+#sub diff_array
+#{
+#	my ($arr_a, $arr_b) = (shift, shift);
+#	return 1
+#		if (@$arr_a != @$arr_b);
+#	my ($i);
+#	$i = 0;
+#	while ($i < @$arr_a)
+#	{
+#		if (!$arr_a->[$i] && !$arr_b->[$i])
+#		{
+#			$i++; next
+#		}
+#		elsif (!$arr_a->[$i] || !$arr_b->[$i])
+#		{
+#			return 1
+#		}
+#		elsif ($arr_a->[$i] ne $arr_b->[$i])
+#		{
+#			return 1
+#		}
+#		else
+#		{
+#			$i++
+#		}
+#	}
+#	return 0
+#}
+
+my $session = 'default';
+
+# add option to pass additional args,
+# default scum behaviour would be a fairly oldskool setup
+#   - int >= 17
+#   - EITHER poly wand or ring of control (poly ring sucks)
+#   - tonal necessary
+# i may change these defaults to something else like
+#  digging + teleport control, or digging + polyitem
+#  tonal + hungerless casting is a bit overrated
+#
+# but, various flags passed to the script could change this
+#   "notonal"  - remove requirement to start with tonal
+#   "nopoly"   - don't scum for a polyitem necessarily
+#   "noint"    - remove minimum int requirement of 17
+#
+#   flags beginning with - are hard scumming requirements,
+#   tho if the prefix is missing, -flag behaviour is assumed
+#   "-marker"   - scum for a marker
+#   "-foodless" - scum for ring of slow digestion
+#   "-digging"  - scum for starting digging wand
+#   "-tc"       - scum for teleport control
+#   
+#   the same flags, if marked with +, are "soft-scum",
+#   i.e. they don't all have to hit, but if any one of them
+#   does, the script stops
+#
+# most options are superceded if a wand is found in the first room,
+# since its worth checking whether it's a wand of wishing
+#
+# screen session can be set with -session=bar
+
+my @scum_keys = ('tonal', 'poly', 'marker', 'foodless', 'digging', 'tc', 'int');
+my @hard_scum = ('poly', 'tonal', 'int');
+my @soft_scum = ();
+my @non_scum = (); # anything added to this list will result in them being removed from 
+foreach my $flag (@ARGV) {
+	if ($flag =~ m/^-h(elp)?$/) {
+		# print usage text and exit
+		print STDERR $usage_text;
+		exit 0;
+	} elsif ($flag =~ m/^-session=([\w-]+)$/) {
+		$session = $1;
+	} elsif ($flag =~ m/^(no|\+|-|.*?)(\w+)$/) {
+        my ($flag_type, $item) = ($1, $2);
+        if (any { $_ eq $item } @scum_keys) {
+            if ($flag_type eq 'no') {
+                push @non_scum, $item;
+            } elsif ($flag_type eq '+') {
+                push @soft_scum, $item;
+            } elsif ($flag_type eq '-' || $flag_type eq '') {
+                push @hard_scum, $item;
+            } else {
+                print STDERR "unrecognised prefix $flag_type (argument $flag)\n";
+            }
+        } else {
+            print STDERR "unrecognised option $item (argument $flag)\n";
+        }
+    } else {
+        print STDERR "unexpected argument format: '$flag'\n";
+    }
+}
+# do some cleaning up to make sure the lists @hard_scum, @soft_scum and @non_scum
+# are free from duplicates, then ensure any item found in @non_scum is not
+# present in hard scum. items in @hard_scum should also supercede any in @soft_scum
+@hard_scum = uniq @hard_scum;
+@soft_scum = uniq @soft_scum;
+@non_scum = uniq @non_scum;
+@hard_scum = negate_filter (\@non_scum, \@hard_scum);
+@soft_scum = negate_filter (\@hard_scum, \@soft_scum);
+# a single item in @soft_scum will behave effectively like a hard scum
+
+# for testing this bit out now just print the lists and exit
+#print STDERR "non-scum list: " . join (", ", @non_scum) . "\n";
+#print STDERR "hard-scum list: " . join (", ", @hard_scum) . "\n";
+#print STDERR "soft-scum list: " . join (", ", @soft_scum) . "\n";
+#exit 0;
+
+my $t0 = [gettimeofday];
+my $count = 0;
+my $timeout = 0;
+my @old_data;
+
+while (1)
 {
-	my ($arr_a, $arr_b) = (shift, shift);
-	return 1
-		if (@$arr_a != @$arr_b);
-	my ($i);
-	$i = 0;
-	while ($i < @$arr_a)
+    my %scum = ();
+	my @data;
+
+	# get screen
+	do
 	{
-		if (!$arr_a->[$i] && !$arr_b->[$i])
-		{
-			$i++; next
+		# this sleep is important for two reasons
+		# one: if it's shorter than the latency, the script will
+		# respond to prompts twice (this behaviour could be changed,
+		# but the current behaviour is much simpler to program)
+		# two: a bug in dgl on the hardfought servers does not deal
+		# gracefully if a dumplog already exists for the current
+		# unix timestamp, which will kill our session
+		# so scumming is effectively capped at one game per second
+        usleep 500000;
+
+		@data = get_screencopy ($session)
+	}
+	while (!@data);
+
+	# start game
+	if (grep { m/\bConnection closed by foreign host\b/ } @data) {
+		my $elapsed = get_elapsed ($t0);
+		die "Spamfiltered ($count tries, time: $elapsed."
+	}
+	
+	# the logic for dealing with start-game and quit prompts
+	# is a bit hacky and may be highly dependent on player rcfile!
+	# windowtype curses is a must, and the terminal should be large
+	# enough to fit the entire inventory (perm_invent must be true)
+	# on a single page
+	if (grep { m/\bTHE NOVEMBER NETHACK TOURNAMENT IS LIVE\b/ }  @data)
+	{
+		# hit t to start game
+		system ('screen', '-S', $session, '-X', 'stuff', 't');
+
+	} elsif (grep { m/\bGoodbye \w+ the \w+\.\.\./ } @data) {
+		# space works for both these exit prompts
+		system ('screen', '-S', $session, '-X', 'stuff', ' ');
+
+	} elsif (grep { m/\bBeware, there will be no return\b/ } @data) {
+		# this prompt i think is standard after trying to exit dl1 by <
+		system ('screen', '-S', $session, '-X', 'stuff', 'y');
+		# ths is typically followed by a >> prompt which for some reason
+		# struggles to match, so just sleep briefly then send a space
+		usleep 200000;
+		system ('screen', '-S', $session, '-X', 'stuff', ' ');
+	
+	# perm_invent means we don't need to actually open inventory so it'll be there on the welcome screen
+	# the welcome message remains on screen during quit steps, so process this case last
+	} elsif (grep { m/\bHello \w+, welcome to\b/ } @data) {
+		# max scum one game per second otherwise dgl error turfs us out
+		# tyrec/2020-10-29.08:51:59.ttyrec.gz already exists; do you wish to overwrite (y or n)? 
+		# check for key items
+		foreach my $line (@data) {
+			if ($line =~ m/\bSt:\d+ Dx:\d+ Co:\d+ In:(\d+) Wi:\d+ Ch:\d+\b/) {
+				$scum{int} = $1 if $1 >= 17;
+			}
+
+            # autopickup gems results in a false positive for room_wand,
+            # so filter that text
+            $line =~ s/\bGems\/Stones\b//;
+			if ($line =~ m/(\/|\b\w - a .*wand)/) {
+                print STDERR "room_wand triggered by $1\n";
+                $scum{room_wand} = 1;
+            }
+			
+			# inventory lines are mutually exclusive
+			if ($line =~ m/\b(tooled horn|harp|bugle|flute)\b/) {
+				$scum{tonal} = $1;
+			}
+			elsif ($line =~ m/\bteleport control\b/) {
+				$scum{tc} = 1;
+			}
+            elsif ($line =~ m/\b(ring of polymorph control|wand of polymorph)\b/) {
+                $scum{poly} = $1;
+            }
+            elsif ($line =~ m/\bmagic marker \(0:[6789]/) {
+                $scum{marker} = 1;
+            }
+            elsif ($line =~ m/\bring of slow digestion\b/) {
+                $scum{foodless} = 1;
+            }
+            elsif ($line =~ m/\bwand of digging\b/) {
+                $scum{digging} = 1;
+            }
 		}
-		elsif (!$arr_a->[$i] || !$arr_b->[$i])
+		
+        # declare a hit regardless of other targets if there's a random wand, either
+		# visible on the map or obtained by autopickup on the staircase on t:1
+        last if $scum{room_wand};
+
+		# make an array of what we hit on this game, tbh actual hash was
+		# only required for some verbose logging code that has been removed for now
+        my @scum_keys = keys %scum;
+
+		# verbose debuggy crap
+        #print STDERR "scum keys this round: " . join (", ", @scum_keys) . "\n";
+        if (is_subset (\@hard_scum, \@scum_keys)) {
+            #print STDERR "hits all hard scum reqs\n";
+        }
+        if (scalar (@soft_scum) == 0) {
+            #print STDERR "no soft scum requirements in place\n";
+        } else {
+            #print STDERR "soft scum hits total: " . count_hits (\@soft_scum, \@scum_keys) . "\n";
+        }
+
+		# the actual check for whether all hard targets are met,
+		# and (if there are any), at least one soft target was found
+        last if (is_subset (\@hard_scum, \@scum_keys)
+                 && (scalar (@soft_scum) == 0 
+                     || (count_hits (\@soft_scum, \@scum_keys) >= 1))
+                );
+        
+		$count++;
+		system ('screen', '-S', $session, '-X', 'stuff', "<");
+	} elsif (grep { m/gzip: .*\.ttyrec\.gz/ } @data) {
+		# if this comes up something weird happened
+		print STDERR "got weird gzip ttyrec question from server\n";
+		exit(1);
+	} else {
+		$timeout++;
+		if ($timeout > 15 * 5)
 		{
-			return 1
-		}
-		elsif ($arr_a->[$i] ne $arr_b->[$i])
-		{
-			return 1
-		}
-		else
-		{
-			$i++
+			my $elapsed = get_elapsed ($t0);
+			print @data;
+			die "Timed out ($count tries, time: $elapsed)\n"
 		}
 	}
-	return 0
 }
+
+my $elapsed = get_elapsed ($t0);
+print STDERR "Success ($count tries, time: $elapsed).\n";
+exit 0;
